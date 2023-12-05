4561a20ef39bf070b55c919b224e88e6a33ca411
Craig Payne <craig@paraglidingmap.com>
2023-12-05 11:17:48 +0100

    no message

--- www/AESCrypt.js
+++
@@ -1,9 +0,0 @@
-var exec = require('cordova/exec');
-
-var AESCrypt = {
-    decrypt: function(base64EncodedData, secureKey, successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "AESCrypt", "decrypt", [base64EncodedData, secureKey]);
-    }
-};
-
-module.exports = AESCrypt;
---
+++ www/PgmAESCrypt.js
@@ -0,0 +1,9 @@
+var exec = require('cordova/exec');
+
+var PgmAESCrypt = {
+    decrypt: function(base64EncodedData, secureKey, successCallback, errorCallback) {
+        exec(successCallback, errorCallback, "PgmAESCrypt", "decrypt", [base64EncodedData, secureKey]);
+    }
+};
+
+module.exports = PgmAESCrypt;
