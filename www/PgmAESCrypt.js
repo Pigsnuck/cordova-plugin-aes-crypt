7a07337566f8330bf89e3e4809f8c07c2751a348
Pigsnuck <craig.r.payne@gmail.com>
2025-11-20 13:21:52 +1300

    Implement encrypt function in PgmAESCrypt
    Added encrypt function to PgmAESCrypt for encryption.

--- www/PgmAESCrypt.js
+++ www/PgmAESCrypt.js
@@ -1,9 +1,13 @@
 var exec = require('cordova/exec');
 
 var PgmAESCrypt = {
-    decrypt: function(base64EncodedData, secureKey, successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "PgmAESCrypt", "decrypt", [base64EncodedData, secureKey]);
-    }
+	decrypt: function(base64EncodedData, secureKey, successCallback, errorCallback) {
+		exec(successCallback, errorCallback, "PgmAESCrypt", "decrypt", [base64EncodedData, secureKey]);
+	},
+
+	encrypt: function(plainText, secureKey, successCallback, errorCallback) {
+		exec(successCallback, errorCallback, "PgmAESCrypt", "encrypt", [plainText, secureKey]);
+	}
 };
 
 module.exports = PgmAESCrypt;
